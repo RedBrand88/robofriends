08e526c18809e445db79b029db2161f30c9f7c82
RedBrand88 <brandon@thefoodtruckleague.com>
2020-04-11 11:24:21 -0600

    Updates

---
+++ precache-manifest.f42d3fe4ff9e35f1ddf3687f84696461.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "348e87d5e9a22eeb816dcd30708c8815",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "dd19ad8a2babc5737b09",
+    "url": "/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "951d09711257d7f57bcb",
+    "url": "/robofriends/static/css/main.d8ce7634.chunk.css"
+  },
+  {
+    "revision": "dd19ad8a2babc5737b09",
+    "url": "/robofriends/static/js/2.843b3040.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.843b3040.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "951d09711257d7f57bcb",
+    "url": "/robofriends/static/js/main.1b2b71c3.chunk.js"
+  },
+  {
+    "revision": "dab07940c1980e5aab1e",
+    "url": "/robofriends/static/js/runtime-main.a4afcfec.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/robofriends/static/media/SEGA.82e09f78.TTF"
+  }
+]);
