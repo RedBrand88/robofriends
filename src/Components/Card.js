659c9083adb411269b4d2b4d8298933df39f5e61
RedBrand88 <brandon@thefoodtruckleague.com>
2020-04-11 15:31:06 -0600

    Change size of image in the api call

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Card = (props) => {
     return (
-        <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img alt='robots' src={`https://robohash.org/${props.id}?200x200`} />
+        <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
+            <img alt='robots' src={`https://robohash.org/${props.id}?size=200x200`} />
             <div>
                 <h2>{props.fullName}</h2>
                 <p>{props.email}</p>
